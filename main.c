6ace53940475f42d8b901cd4c6c96c8d430e6f81
Zoltan Racz <racz.zoli@gmail.com>
2025-04-12 02:09:42 +0300

    Added copyright notice to main.c

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/* 
+	Copyright (C) 2025 Zoltan Racz
+
+   	This program is free software; you can redistribute it and/or modify
+   	it under the terms of the GNU General Public License as published by
+   	the Free Software Foundation, version 2 of the License.
+	
+   	This program is distributed in the hope that it will be useful,
+   	but WITHOUT ANY WARRANTY; without even the implied warranty of
+   	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   	GNU General Public License for more details.
+	
+   	You should have received a copy of the GNU General Public License
+   	along with this program.  If not, see <https://www.gnu.org/licenses/>.  
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -221,7 +237,7 @@ void sort_dentries(struct dir_entry *head, int depth)
 	int i;
 
 	/**
-	** we only sort the the children displayed.
+	** we only sort the the displayed children.
 	** ex. if --max-depth=1 then we sort only the first level of children, 
 	** since the rest is not displayed, no need to sort it
 	**/
