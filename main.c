91bd230e5372e9edad54903debae918312751b1b
Zoltan Racz <racz.zoli@gmail.com>
2025-04-14 01:12:38 +0300

    Changed my name in the copyright notice :)

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2025 Zoltan Racz
+ * Copyright (C) 2025 Zoltán Rácz
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
